6aed1a1b2006b30dfc2e6da2aafd5ccbd1b43452
Aniket Bonagir <abonagir@qualys.com>
2022-10-08 08:56:25 +0530

    software modelling ex 1

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 import './style.css';
-import { createMachine } from 'xstate';
+// import { createMachine } from 'xstate';
+
+
+function transition(state, event) {
+  switch(event.type) {
+    case "FETCH":
+      console.log("Starting to fetch Data");
+      break;
+    default:
+      break;
+  }
+}
+  
+window.transition = transition;
